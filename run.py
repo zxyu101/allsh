38287f45c7579b49d96397da08f064e74151c09f
zxyu101 <42508744+zxyu101@users.noreply.github.com>
2024-03-24 07:27:26 +0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,11 @@
+# @BENN_DEV & @BENfiles
+from Bot import app
+import os
+
+def main():
+    os.system("pip install pyrogram tgcrypto requests bs4 html.parser")
+    print("A LIVE!")
+    app.run()
+
+if __name__ == "__main__":
+    main()
